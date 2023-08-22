add8584bcc5765087eb0b3d03a22a45db33a1ee1
fireattack <human.peng@gmail.com>
2023-08-22 11:28:09 +0800

    Rewrite

--- lastfm_collage_generator.py
+++ lastfm_collage_generator.py
@@ -1,12 +1,80 @@
-import re
+import argparse
+import json
+import ssl
+import webbrowser
 from pathlib import Path
 
+import requests
 import tweepy
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFilter, ImageFont
+from requests.adapters import HTTPAdapter
 from requests_oauthlib import OAuth1
-from util import download, dump_json, requests_retry_session
+from rich.console import Console
+from rich.table import Table
+from urllib3.util.retry import Retry
+from urllib3.util.ssl_ import create_urllib3_context
+
+
+class AdapterFix(HTTPAdapter):
+    def __init__(self, **kwargs):
+        self.ssl_context = create_urllib3_context()
+        self.ssl_context.options &= ~ssl.OP_NO_TICKET
+        super().__init__(**kwargs)
+
+    def init_poolmanager(self, *args, **kwargs):
+        kwargs["ssl_context"] = self.ssl_context
+        super().init_poolmanager(*args, **kwargs)
+
+    def proxy_manager_for(self, *args, **kwargs):
+        kwargs["ssl_context"] = self.ssl_context
+        return super().proxy_manager_for(*args, **kwargs)
+
+
+def requests_retry_session(
+    retries=5,
+    backoff_factor=0.2,
+    status_forcelist=(502, 503, 504),
+    session=None,
+):
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = AdapterFix(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
+
+
+# write a simple download function
+def download(url, f):
+    with requests_retry_session().get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(f, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+
+
+def dump_json(mydict, filename):
+    filename = Path(filename)
+    if filename.suffix.lower() != '.json':
+        filename = filename.with_suffix('.json')
+    filename.parent.mkdir(parents=True, exist_ok=True)
+    with filename.open('w', encoding='utf-8') as f:
+        json.dump(mydict, f, ensure_ascii=False, indent=2)
+
+
+def load_json(filename, encoding='utf-8'):
+    filename = Path(filename)
+    with filename.open('r', encoding=encoding) as f:
+        data = json.load(f)
+    return data
+
 
 API_KEY = 'b7cad0612089bbbfecfc08acc52087f1'
+
 
 def make_square(image, size):
     # Calculate aspect ratio
@@ -34,6 +102,7 @@ def make_square(image, size):
 
     return image
 
+
 def get_info(username, period, limit):
     url = f"http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user={username}&period={period}&api_key={API_KEY}&limit={limit}&format=json"
 
@@ -42,7 +111,30 @@ def get_info(username, period, limit):
 
     return data['topalbums']['album']
 
-def create_collage(data, side_length, rows, cols, show_name, output='collage.jpg'):
+
+def get_font_size(text, side_length):
+    while True:
+        font = ImageFont.truetype('SourceHanSans-Regular.otf', font_size)
+        length = ImageDraw.textlength(text, font=font)
+        if length > side_length:
+            font_size -= 1
+        else:
+            break
+
+    return font_size
+
+
+def add_text(image, draw, x, y, text, font, stroke_width):
+    blurred = Image.new('RGBA', image.size)
+    blurred_draw = ImageDraw.Draw(blurred)
+    blurred_draw.text(xy=(x, y), text=text, fill='white', font=font, anchor="mm", align="center")
+    blurred = blurred.filter(ImageFilter.GaussianBlur(radius=10))
+    # blurred_draw.text(xy=(x+5,y+5), text=text, fill='black', font=font, anchor="mm", align="center")
+    image.paste(blurred, blurred)
+    draw.text((x, y), text, fill="white", font=font, stroke_fill='black', stroke_width=stroke_width, anchor="mm", align="center")
+
+
+def create_collage(data, side_length, rows, cols, show_name, output, show=False):
     # side_length = SIDE_LENGTHS[size]
     collage_width = side_length * cols
     collage_height = side_length * rows
@@ -77,34 +169,47 @@ def create_collage(data, side_length, rows, cols, show_name, output='collage.jpg
                     textY = i * side_length + side_length // 2
 
                     for idx, text in enumerate([album_name, artist_name]):
-                        local_max_fontsize = max_fontsize if idx == 0 else round(max_fontsize / 1.2) # artist line is smaller
-                        fontsize = (min(int((side_length * 1.2) // len(text)), local_max_fontsize))
+                        local_max_fontsize = max_fontsize if idx == 0 else round(max_fontsize / 1.2)  # artist line is smaller
+
+                        fontsize = local_max_fontsize
+                        while True:
+                            font = ImageFont.truetype('SourceHanSans-Regular.otf', fontsize)
+                            length = draw.textlength(text, font=font)
+                            if length > side_length * 0.95:  # leave some margin
+                                fontsize -= 1
+                            else:
+                                break
+
                         if fontsize != local_max_fontsize:
                             print(f'[DEBUG] long text: {text}, fontsize: {fontsize}')
-                        font = ImageFont.truetype('SourceHanSans-Regular.otf', fontsize)
+
                         textY_shift = textY - line_height // 2 + line_height * idx
-                        draw.text((textX, textY_shift), text, fill="white", font=font, stroke_fill='black', stroke_width=stroke_width, anchor="mm", align="center")
+                        add_text(collage, draw, textX, textY_shift, text, font, stroke_width)
+                        # draw.text((textX, textY_shift), text, fill="white", font=font, stroke_fill='black', stroke_width=stroke_width, anchor="mm", align="center")
 
     collage.save(output, format='JPEG', quality=94, optimize=True, subsampling=0)
+    if show:
+        collage.show()
 
-def load_keys():
-    # format: consumer_key, consumer_secret, access_token, access_token_secret, each on a line
-    auth_file = Path('auth_twitter.txt')
-    with auth_file.open('r') as f:
-        return f.read().splitlines()
 
 def tweet(text, file):
+    def load_keys():
+        # format: consumer_key, consumer_secret, access_token, access_token_secret, each on a line
+        auth_file = Path('auth_twitter.txt')
+        with auth_file.open('r') as f:
+            return f.read().splitlines()
+
     keys = load_keys()
-    tweepy_auth = tweepy.OAuth1UserHandler(
-        *keys
-    )
+    tweepy_auth = tweepy.OAuth1UserHandler(*keys)
     tweepy_api = tweepy.API(tweepy_auth)
-    post = tweepy_api.simple_upload(file)
-    text = str(post)
-    print(f'[DEBUG] twitter media upload response: {text}')
-    media_id = re.search(r"media_id=(.+?),", text)[1]
+
+    tweepy_api.session = requests_retry_session()
+
+    media = tweepy_api.simple_upload(file)
+    print(f'[DEBUG] twitter media upload response: {media}')
+    media_id = media.media_id
     print(f'[DEBUG] media_id: {media_id}')
-    payload = {"media": {"media_ids": ["{}".format(media_id)]}}
+    payload = {"media": {"media_ids": [str(media_id)]}}
     payload['text'] = text
     dump_json(payload, 'payload.json')
 
@@ -112,21 +217,46 @@ def tweet(text, file):
     print(f'[DEBUG] twitter tweet status code: {r.status_code}')
     print(f'[DEBUG] twitter tweet response: {r.text}')
 
-def main():
-    username = 'fireattack'
-    period = '7day' # 7day | 1month | 3month | 6month | 12month | overall
-    rows = 3
-    cols = 3
-    show_name = True
-    size = 500
-
-    data = get_info(username, period, limit=rows*cols)
-
-    output = f'collage_{size}.jpg'
-    create_collage(data, size, rows, cols, show_name, output=output)
-
-    text = f'My fav albums this week https://last.fm/user/{username}'
-    tweet(text, output)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('action', default='fetch', choices=['fetch', 'collage', 'tweet', 'all'], nargs='?')
+    parser.add_argument('--username', type=str, default='fireattack')
+    parser.add_argument('--period', type=str, default='7day', choices=['7day', '1month', '3month', '6month', '12month', 'overall'])
+    parser.add_argument('--rows', type=int, default=3)
+    parser.add_argument('--cols', type=int, default=3)
+    parser.add_argument('--show_name', action='store_true', default=True)
+    parser.add_argument('--size', type=int, default=500)
+
+    args = parser.parse_args()
+    output = f'collage_{args.size}.jpg'
+
+    if args.action in ['fetch', 'all']:
+        data = get_info(args.username, args.period, limit=args.rows*args.cols+3)  # get a little bit more
+        dump_json(data, 'data.json')
+        # print info
+        table = Table(show_header=True, header_style="bold magenta")
+        table.add_column("Rank", style="dim", width=5)
+        table.add_column("Album")
+        table.add_column("Artist")
+        table.add_column("Playcount", justify="right")
+        table.add_column("Has cover")
+        for i, album in enumerate(data):
+            if album['image'][-1]['#text']:
+                has_cover = '✅'
+            else:
+                has_cover = '❌'
+                webbrowser.open(album['url'])
+            table.add_row(str(i+1), album['name'], album['artist']['name'], str(album['playcount']), has_cover)
+        console = Console()
+        console.print(table)
+
+    if args.action in ['collage', 'all']:
+        data = load_json('data.json')
+        show = True if args.action == 'collage' else False  # show only if action is collage
+        create_collage(data, args.size, args.rows, args.cols, args.show_name, output, show)
+
+    if args.action in ['tweet', 'all']:
+        text = f'My fav albums this week https://last.fm/user/{args.username}'
+        assert Path(output).exists(), f'Collage {output} not found'
+        tweet(text, output)
